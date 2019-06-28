65a9f6ccc575b072596c8dbfa39788352d04be6c
unknown <anuplonappan@gmail.com>
2019-06-28 10:36:36 +0200

    adds missing files

---
+++ customer-cessation-service-adapter/src/main/java/com/asyncstream/cloudmessage/bpm/customercessation/service/impl/CustomerCessationServiceImpl.java
@@ -0,0 +1,12 @@
+package com.asyncstream.cloudmessage.bpm.customercessation.service.impl;
+
+import com.asyncstream.cloudmessage.bpm.customercessation.service.api.CessationRequest;
+import com.asyncstream.cloudmessage.bpm.customercessation.service.api.CessationResult;
+import com.asyncstream.cloudmessage.bpm.customercessation.service.api.CustomerCessationService;
+
+public class CustomerCessationServiceImpl implements CustomerCessationService {
+    @Override
+    public CessationResult cease(CessationRequest request) {
+        return null;
+    }
+}
